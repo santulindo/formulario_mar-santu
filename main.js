be458cf53e58b1e4544e763606fe232c0f508bf9
JuanPabloBeltran <juanpablobeltranrodriguez@gmail.com>
2025-08-01 11:37:19 -0500

    funcion modal asi se rie aaa Jjajaj

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import {showModal, h1demodal} from './modal.js';
 document.addEventListener('DOMContentLoaded', () => { 
-    Event.preventDefault();
-    showModal();
-} );
-document.getElementById('close-modal').addEventListener('click', () => {
-    h1demodal();
+    const form = document.getElementById('formulario');
+    const submitButton = document.getElementById('submit');
+    const closeModalButton = document.getElementById('close-modal');
+
+    form.addEventListener('submit', (event) => {
+        e.preventDefault();
+        showModal();
+    });
+
+    closebtn.addEventListener('click', h1deModal);
 });
